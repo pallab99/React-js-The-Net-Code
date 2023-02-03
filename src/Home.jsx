0c4b57ca2cbd198405fa3f33825e12a2a113394a
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-03 17:06:10 +0600

    Usestate Hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,18 @@
+import { useState } from "react";
 const Home = () => {
+  const [name, setName] = useState("Pallab");
+  const [age,setAge]=useState(20);
+  
   const handleClick = () => {
-    console.log("Hello World");
+    setName("Pallab Majumdar");
+    setAge(23);
   };
 
-  const handleClickAgain = (name) => {
-    console.log("hello " + name);
-  };
   return (
     <div className="Home">
       <h1>Home Page</h1>
+      <p>{name} is {age} years old</p>
       <button onClick={handleClick}>Click me</button>
-      <button
-        onClick={() => {
-          handleClickAgain("Pallab");
-        }}
-      >
-        Click Me Again
-      </button>
     </div>
   );
 };
