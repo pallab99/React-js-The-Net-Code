f3337ffc7839a4078ddf8cd4b5564cf05cdf1e0d
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-03 16:46:52 +0600

    Handle The click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,24 @@
 const Home = () => {
-    return (
-        <div className="Home">
-            <h1>Home Page</h1>
-        </div>
-    );
-}
- 
+  const handleClick = () => {
+    console.log("Hello World");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("hello " + name);
+  };
+  return (
+    <div className="Home">
+      <h1>Home Page</h1>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={() => {
+          handleClickAgain("Pallab");
+        }}
+      >
+        Click Me Again
+      </button>
+    </div>
+  );
+};
+
 export default Home;
