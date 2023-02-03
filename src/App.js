e33ea009e1af2f4f47d5cd7e004f2812b3536fb5
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-03 12:11:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pallab Majumdar
         </a>
       </header>
     </div>
