639829658d3519fc7ef6ee2a4bf66fbda2bc2c3d
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-03 14:03:49 +0600

    Hello

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import "./App.css";
-
+import Navbar from "./Navbar";
+import Home from "./Home";
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link="https://www.youtube.com/";
   return (
     <div className="App">
-      <div className="container">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{10}</p>
-        <p>{"Hello,everyone"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}>Youtube</a>
+      <Navbar />
+      <div className="content">
+        <Home />
       </div>
     </div>
   );
