6edf7abac791d9dfe79c3ac9028b596a917eec41
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-04 23:21:31 +0600

    Reuse the components

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,12 +5,21 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {title: "Web dev top tips",body: "lorem ipsum...",author: "mario",id: 3,}
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
   ]);
 
   return (
     <div className="Home">
-      <BlogList blogs={blogs} title="All Blogs!"/>
+      <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList
+        blogs={blogs.filter((blogs) => blogs.author === "mario")}
+        title="Mario's Blogs!"
+      />
     </div>
   );
 };
