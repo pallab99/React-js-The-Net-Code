a533706039d4f7aa4ffb3c35d0bd6406469a7f07
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-06 01:52:21 +0600

    Use Custom Hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,53 +1,9 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
-import axios from "axios";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [ispending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
 
-  // const getData = () => {
-  //   setTimeout(() => {
-  //     fetch("http://localhost:8000/blogs")
-  //       .then((res) => {
-  //         if (!res.ok) {
-  //           throw Error("Couldn't connect to the server");
-  //         }
-  //         return res.json();
-  //       })
-  //       .then((data) => {
-  //         setBlogs(data);
-  //         setIsPending(false);
-  //         setError(null);
-  //       })
-  //       .catch((err) => {
-  //         setIsPending(false);
-  //         setError(err.message);
-  //       });
-  //   }, 1000);
-  // };
-
-  const getData = () => {
-    setTimeout(() => {
-      axios({
-        url: "http://localhost:8000/blogs",
-      })
-        .then((response) => {
-          setBlogs(response.data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
+  const {data:blogs,ispending,error}=useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="Home">
---
+++ src/useFetch.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [ispending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios({
+        url: url,
+      })
+        .then((response) => {
+          setData(response.data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, [url]);
+
+  return { data, ispending, error };
+};
+
+export default useFetch;
