98df8706ed4643c00d883b32d409c9ffbe79c658
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-06 00:23:05 +0600

    Axios is awesome

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,46 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import axios from "axios";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-  // const [name, setName] = useState("mario");
   const [ispending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  // const getData = () => {
+  //   setTimeout(() => {
+  //     fetch("http://localhost:8000/blogs")
+  //       .then((res) => {
+  //         if (!res.ok) {
+  //           throw Error("Couldn't connect to the server");
+  //         }
+  //         return res.json();
+  //       })
+  //       .then((data) => {
+  //         setBlogs(data);
+  //         setIsPending(false);
+  //         setError(null);
+  //       })
+  //       .catch((err) => {
+  //         setIsPending(false);
+  //         setError(err.message);
+  //       });
+  //   }, 1000);
+  // };
 
   const getData = () => {
-    
     setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          return res.json();
-        })
-
-        .then((data) => {
-          setBlogs(data);
+      axios({
+        url: "http://localhost:8000/blogs",
+      })
+        .then((response) => {
+          setBlogs(response.data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   };
@@ -27,17 +51,9 @@ const Home = () => {
 
   return (
     <div className="Home">
+      {error && <div>{error}</div>}
       {ispending && <div className="loading">Loading....</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-
-      {/* <button
-        onClick={() => {
-          setName("pallab");
-        }}
-      >
-        Change Name
-      </button>
-      <p>{name}</p> */}
     </div>
   );
 };
